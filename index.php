6899c099147dd8c4007395908a2025353fc7a61f
githubfollowme <flymayday112@gmail.com>
2022-01-03 16:28:22 +0800

    完成登出功能

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ include_once "base.php";
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 	<title>健康促進網</title>
-	<link href="./home_files/css.css" rel="stylesheet" type="text/css">
-	<script src="./home_files/jquery-1.9.1.min.js"></script>
-	<script src="./home_files/js.js"></script>
+	<link href="./css/css.css" rel="stylesheet" type="text/css">
+	<script src="./js/jquery-1.9.1.min.js"></script>
+	<script src="./js/js.js"></script>
 </head>
 
 <body>
@@ -37,7 +37,26 @@ include_once "base.php";
 					<!-- 因為還有預設的padding 所以marquee設82的話 會把會員登入擠下去 -->
 					<marquee style="width:78%;display:inline-block;">請民眾踴躍投稿,讓電子報成為大家相互交流`分享的的園地,詳見最新文章</marquee>
 					<span style="width:18%; display:inline-block;">
-						<a href="?do=login">會員登入</a>
+					<?php
+					if(isset($_SESSION['login'])){
+
+						if($_SESSION['login']=='admin'){
+						?>
+							歡迎admin，<br><button>管理</button>|<button onclick='logout()'>登出</button>
+						<?php
+						}else{
+						?>
+							歡迎<?=$_SESSION['login'];?>，<button onclick='logout()'>登出</button>
+						<?php
+						}
+					?>
+					<?php
+					}else{
+					?>
+						<a href="?do=login">會員登入</a>					
+					<?php
+					}
+					?>
 					</span>
 					<div class="">
 						<?php
