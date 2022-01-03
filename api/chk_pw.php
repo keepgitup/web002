7d685fe6096f7b4a43cf2b36a82f5e952e19ace4
githubfollowme <flymayday112@gmail.com>
2022-01-03 15:44:54 +0800

    完成登入檢查

--- api/chk_pw.php
+++ api/chk_pw.php
@@ -1,1 +1,16 @@
-<?php include_once "../base.php";?>
+<?php include_once "../base.php";
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$chk=$User->math('count','*',['acc'=>$acc,'pw'=>$pw]);
+
+//echo ($chk>0)?1:0;
+if($chk>0){
+    // 表帳號密碼存在
+    echo 1;
+$_SESSION['login']=$acc;
+}else{
+    // 表帳號密碼錯誤
+    echo 0;
+}
--- api/find_pw.php
+++ api/find_pw.php
@@ -1,5 +1,9 @@
 <?php include_once "../base.php";
 $email=$_POST['email'];
+// 'email'=>$email] 這事鍵值陣列，所以應該是
+// 對應到find的函數
+// 針對這個鍵值陣列
+// 將資料表的email 欄位 比對 $email變數 後將查詢到的結果回傳給 $user
 $user=$User->find(['email'=>$email]);
 
 if(empty($user)){
