9b61b76509b7867336acdba09c0a6aec4af74c14
githubfollowme <flymayday112@gmail.com>
2022-01-03 10:57:35 +0800

    完成瀏灠人次顯示及紀錄

--- front/header.php
+++ front/header.php
@@ -1,5 +1,7 @@
 <div id="title">
-			<?=date("m月d號l");?> | 今日瀏覽: 1 | 累積瀏覽: 36
+			<?=date("m月d號l");?> |
+             今日瀏覽:<?=$View->find(['date'=>date("Y-m-d")])['total'];?> |
+             累積瀏覽:<?=$View->math('sum','total');?>
 		<a href="/index.php">回首頁</a>
 <!-- div下移 包住index.php -->
     </div>
