df199d5d8aef253a6565bb0fa5f3537d5731e46a
githubfollowme <flymayday112@gmail.com>
2022-01-03 16:36:08 +0800

    完整切出頁首和頁尾區塊

--- front/header.php
+++ front/header.php
@@ -2,10 +2,10 @@
 			<?=date("m月d號l");?> |
              今日瀏覽:<?=$View->find(['date'=>date("Y-m-d")])['total'];?> |
              累積瀏覽:<?=$View->math('sum','total');?>
-		<a href="/index.php">回首頁</a>
-<!-- div下移 包住index.php -->
-    </div>
-        <div id="title2">
-			<a href="/index.php" title="健康促進網-回首頁"></a>
-
+             <a href='index.php' style='float:right'>回首頁</a>
 		</div>
+        <div id="title2">
+        	<a href="index.php" title="健康促進網-回首頁">
+				<img src="icon/02B01.jpg" alt="">
+			</a>
+        </div>
