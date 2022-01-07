f9dc2224ac57ad7944930f46bff862a5e8d3af29
githubfollowme <flymayday112@gmail.com>
2022-01-07 13:09:51 +0800

    完成主題內容頁籤功能

--- front/home.php
+++ front/home.php
@@ -20,12 +20,12 @@
 }
 </style>
 <div style="display:flex;margin-left:1px">
-  <div class="tag">健康新知</div>
-  <div class="tag">菸害防治</div>
-  <div class="tag">癌症防治</div>
-  <div class="tag">慢性病防治</div>
+  <div class="tag active" id='t1'>健康新知</div>
+  <div class="tag" id='t2'>菸害防治</div>
+  <div class="tag" id='t3'>癌症防治</div>
+  <div class="tag" id='t4'>慢性病防治</div>
 </div>
-<div class="post">
+<div class="post" id="p1">
   <h1>健康新知</h1>
 <pre>
 缺乏運動已成為影響全球死亡率的第四大危險因子-國人無規律運動之比率高達72.2%
@@ -48,19 +48,18 @@
 
 </pre>
 </div>
-<div class="post">2</div>
-<div class="post">3</div>
-<div class="post">4</div>
+<div class="post" id="p2">2</div>
+<div class="post" id="p3">3</div>
+<div class="post" id="p4">4</div>
 
 <script>
-$(".post").first().show()
-$(".tag").first().addClass('active')
+$("#p1").show();
 $(".tag").on("click",function(){
-    let idx=$(this).index()
+    let id=$(this).attr('id').replace("t","p")
     $(".tag").removeClass("active")
     $(this).addClass('active')
     $(".post").hide()
-    $(".post").eq(idx).show()
+    $("#"+id).show()
 })
 
 </script>
--- front/news.php
+++ front/news.php
@@ -1,1 +1,33 @@
-test
+<div>目前位置:首頁>最新文章區</div>
+<fieldset>
+<legend>最新文章區</legend>
+<table>
+<tr>
+    <td width="30%">標題</td>
+    <td width="50%">內容</td>
+</tr>
+<?php
+$total=$News->math("count","*");
+$div=5;
+$pages=ceil($total/$div);
+$now=$_GET['p']??1;
+//從哪一筆資料開始撈 
+$start=($now-1)*$div;
+$rows=$News->all(['sh'=>1],"limit $start,$div");
+foreach ($rows as $key=>$row){}
+?>
+<!-- 要準備用substring了 才能抓到文章一些內容 -->
+<tr>
+    <td class="switch"><?=($row['title'];?></td>
+    <td class="switch">
+<div class="short"><?=?></div>
+<div class="full"></div>
+
+    </td>
+    
+    
+</tr>
+
+</table>
+
+</fieldset>
