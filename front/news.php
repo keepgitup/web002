0d06bcbc6336e8ec592a8ddca67f48049e364816
githubfollowme <flymayday112@gmail.com>
2022-01-07 13:18:45 +0800

    完成最新文章及人氣文章共同的部份

--- front/news.php
+++ front/news.php
@@ -1,33 +1,65 @@
-<div>目前位置:首頁>最新文章區</div>
 <fieldset>
-<legend>最新文章區</legend>
+    <legend>目前位置：首頁 > 最新文章區</legend>
 <table>
-<tr>
-    <td width="30%">標題</td>
-    <td width="50%">內容</td>
-</tr>
-<?php
-$total=$News->math("count","*");
-$div=5;
-$pages=ceil($total/$div);
-$now=$_GET['p']??1;
-//從哪一筆資料開始撈 
-$start=($now-1)*$div;
-$rows=$News->all(['sh'=>1],"limit $start,$div");
-foreach ($rows as $key=>$row){}
-?>
-<!-- 要準備用substring了 才能抓到文章一些內容 -->
-<tr>
-    <td class="switch"><?=($row['title'];?></td>
-    <td class="switch">
-<div class="short"><?=?></div>
-<div class="full"></div>
+    <tr>
+        <td width="30%">標題</td>
+        <td width="50%">內容</td>
+        <td></td>
+    </tr>
+    <?php
 
-    </td>
-    
-    
-</tr>
+    $total=$News->math("count","*");
+    $div=5;
+    $pages=ceil($total/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
 
+    $rows=$News->all(['sh'=>1]," limit $start,$div");
+    foreach ($rows as $key => $row) {
+    ?>
+    <tr>
+        <td class="switch"><?=$row['title'];?></td>
+        <td class="switch">
+            <div class="short"><?=mb_substr($row['text'],0,20);?>...</div>
+            <div class="full" style="display:none"><?=nl2br($row['text']);?></div>
+        </td>
+        <td></td>
+    </tr>
+    <?php
+    }
+    ?>
 </table>
+<div>
+<?php
 
+if(($now-1)>0){
+    $prev=$now-1;
+    echo "<a href='index.php?do=news&p=$prev'> ";
+    echo " < ";
+    echo " </a>";
+}
+
+
+for($i=1;$i<=$pages;$i++){
+    $font=($now==$i)?'24px':'16px';
+    echo "<a href='index.php?do=news&p=$i' style='font-size:$font'> ";
+    echo $i;
+    echo " </a>";
+}
+
+if(($now+1)<=$pages){
+    $next=$now+1;
+    echo "<a href='index.php?do=news&p=$next'> ";
+    echo " > ";
+    echo " </a>";
+}
+
+?>
+</div>
 </fieldset>
+<script>
+$(".switch").on("click",function(){
+    $(this).parent().find(".short,.full").toggle()
+})
+
+</script>
--- front/pop.php
+++ front/pop.php
@@ -1,1 +1,68 @@
-test
+<fieldset>
+    <legend>目前位置：首頁 > 人氣文章區</legend>
+<table>
+    <tr>
+        <td width="30%">標題</td>
+        <td width="50%">內容</td>
+        <td>人氣</td>
+    </tr>
+    <?php
+
+    $total=$News->math("count","*");
+    $div=5;
+    $pages=ceil($total/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
+
+    $rows=$News->all(['sh'=>1]," order by `good` desc limit $start,$div");
+    foreach ($rows as $key => $row) {
+    ?>
+    <tr>
+        <td class="switch"><?=$row['title'];?></td>
+        <td class="switch">
+            <div class="short"><?=mb_substr($row['text'],0,20);?>...</div>
+            <div class="full" style="display:none"><?=nl2br($row['text']);?></div>
+        </td>
+        <td>
+            <?=$row['good'];?>個人說<img src='icon/02B03.jpg' style='width:25px'>
+        </td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+<div>
+<?php
+
+if(($now-1)>0){
+    $prev=$now-1;
+    echo "<a href='index.php?do=news&p=$prev'> ";
+    echo " < ";
+    echo " </a>";
+}
+
+
+for($i=1;$i<=$pages;$i++){
+    $font=($now==$i)?'24px':'16px';
+    echo "<a href='index.php?do=news&p=$i' style='font-size:$font'> ";
+    echo $i;
+    echo " </a>";
+}
+
+if(($now+1)<=$pages){
+    $next=$now+1;
+    echo "<a href='index.php?do=news&p=$next'> ";
+    echo " > ";
+    echo " </a>";
+}
+
+?>
+</div>
+</fieldset>
+<script>
+$(".switch").hover(
+    function(){
+    $(this).parent().find(".short,.full").toggle()
+})
+
+</script>
