9a049fe41f23362b40e3dd2f51c031fac81ed048
githubfollowme <flymayday112@gmail.com>
2022-01-10 10:58:54 +0800

    列出問卷題目

--- front/que.php
+++ front/que.php
@@ -1,25 +1,42 @@
 <fieldset>
-    <form action="">
+    <legend>目前位置：首頁>問卷調查</legend>
+<table class='tab'>
+    <tr>
+        <td width="10%">編號</td>
+        <td width="50%">問卷題目</td>
+        <td width="15%">投票總數</td>
+        <td width="10%">結果</td>
+        <td>狀態</td>
+    </tr>
+    <?php
+    $ques=$Que->all(['parent'=>0]);
+    foreach($ques as $key => $que){
+    ?>
+    <tr>
+        <td><?=$key+1;?></td>
+        <td><?=$que['text'];?></td>
+        <td><?=$que['count'];?></td>
+        <td>
+            <a href='?do=result&id=<?=$que['id'];?>'>結果</a>
+        </td>
+        <td>
+            <?php
+                if(isset($_SESSION['login'])){
+                    echo "<a href='?do=vote&id={$que['id']}'>";
+                    echo "參與投票";
+                    echo "</a>";
 
-        <div>
-            <input type="text" name="subject">
-        </div>
-        <div class="clo" id="opt">
-            <div><span>選項</span>
-                <input type="text" name="options[]">
-                <input type="button" onlick="more()" value="更多">
+                }else{
+                    echo "請先登入";
+                }
+            ?>
+        </td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
 
-            </div>
 
-        </div>
-        <div>
-            <input type="submit" value="新增">
-            <input type="reset" value="清空">
-        </div>
 
-    </form>
 </fieldset>
-
-<script>
-    function more()
-</script>
