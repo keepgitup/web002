e7b64049986ef1016f3869f23da78dc7b23e3864
githubfollowme <flymayday112@gmail.com>
2022-01-10 10:42:21 +0800

    完成後台新增問卷功能

--- front/que.php
+++ front/que.php
@@ -1,1 +1,25 @@
-test
+<fieldset>
+    <form action="">
+
+        <div>
+            <input type="text" name="subject">
+        </div>
+        <div class="clo" id="opt">
+            <div><span>選項</span>
+                <input type="text" name="options[]">
+                <input type="button" onlick="more()" value="更多">
+
+            </div>
+
+        </div>
+        <div>
+            <input type="submit" value="新增">
+            <input type="reset" value="清空">
+        </div>
+
+    </form>
+</fieldset>
+
+<script>
+    function more()
+</script>
